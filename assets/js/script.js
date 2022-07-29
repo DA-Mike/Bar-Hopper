aca420f22c7c446f6d8f19a814b409456e13ea61
Mike Rugh <mike.rugh@gmail.com>
2022-07-28 17:22:19 -0700

    adds locational solving functions and cleans up code

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,8 +13,12 @@ var geoObj = JSON.parse(localStorage.getItem("geoObj"));
 var yelpObj = JSON.parse(localStorage.getItem("yelpObj"));
 // var startLat = 37.787047;
 // var startLong = -122.401239;
-// var endLat = 37.78658629651325;
-// var endLong = -122.4045181274414;
+var endLat = 37.78665355217418;
+var endLong = -122.40389365795478;
+var yelpStartPoint = [];
+var shortList = [];
+var candidates = [];
+var yelpStart = [];
 
 function formSubmitHandler(distance) {
     meters = distance * 1609;
@@ -23,7 +27,7 @@ function formSubmitHandler(distance) {
 
 //retrieves yelp api response
 function getStartPoints(address, meters){
-    var myurl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=bars&location=" + address + "&radius=" + meters;
+    var myurl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=bars&location=" + address + "&radius=" + meters + '&limit=30';
 
     $.ajax({
     url: myurl,
@@ -53,9 +57,7 @@ function getStartPoints(address, meters){
 
 // getStartPoints(address, meters);
 
-//take yelp response and run coordinates through mapping api to find bars at max distance
-
-
+//get route
 function getRoute(startLat, startLong, endLat, endLong){
     // var apiUrl = 'https://cors-anywhere.herokuapp.com/https://api.openrouteservice.org/v2/directions/foot-walking?api_key=' + orsApiKey + '&start=' + startLong + "," + startLat + '&end=' + endLong + "," + endLat + '&units=m';
     var apiUrl = 'https://api.geoapify.com/v1/routing?waypoints=' + startLat + ',' + startLong + '|' + endLat + ',' + endLong + '&mode=walk&apiKey=' + geoKey;
@@ -80,65 +82,131 @@ function getRoute(startLat, startLong, endLat, endLong){
 
 // getRoute(startLat, startLong, endLat, endLong);
 
+
+//finds bars that are furthest away from endpoint with distance input parameter
 function solveForStartPoints(geo, yelp) {
-    // console.log("data length: ", data.length);
-
-    var geoStart = [];
-    var yelpStart = [];
-
-    // for (i = 0; i < geo.length; i++) { 
-    //     var itDistance = geo[i].features[0].properties.distance;
-    //     // console.log("itDistance: ", itDistance);
-    //     // console.log("Distance: ", data[i].features[0].properties.summary.distance);
-    //     if (itDistance >= (meters * .9) || itDistance >= (meters * .65) || itDistance >= (meters * .5)) {
-    //         // orsStart.push(ors[i].features[0].properties.summary.distance);
-    //         geoStart.push(geo[i]);
-    //         yelpStart.push(yelp[0].businesses[i]);
-    //     }
-    // }
 
     for (i = 0; i < yelp[0].businesses.length; i++) {
-        var itDistance = yelp[0].businesses[i].distance; 
-        console.log("distance: ", yelp[0].businesses[i].distance);
+        var itDistance = yelp[0].businesses[i].distance;
         if (itDistance >= (meters * .9) || itDistance >= (meters * .65) || itDistance >= (meters * .5)) {
-                    // orsStart.push(ors[i].features[0].properties.summary.distance);
-                // geoStart.push(geo[i]);
             yelpStart.push(yelp[0].businesses[i]);
+        } else if (itDistance <= (meters * .1)) {
+            //creates short distance list in case we solve for less than desired number of bars
+            shortList.push(yelp[0].businesses[i]);
         }
     }
-
     console.log("yelpStart: ", yelpStart);
-    // console.log("geoStart: ", geoStart);
-
-    // for (i = 0; i < geoStart.length; i++) {
-    //     yelpStart[i]['ors'] = geoStart[i];
-    // }
-    // console.log("yelpStart: ", yelpStart);
-    // console.log("orsStart: ", orsStart);
-    // for (i = 0; i < yelpStart.length; i++) {
+    
     yelpStart.sort((a, b) => a < b ? 1 : a > b ? -1 : 0);
-    // console.log("ors sorted: ", orsStart);
-    // console.log("yelpStart: ", yelpStart);
-    // for (x = 0; x < yelpStart.length; x++) {
+    
     for (i = 0; i < 3; i++) {
-        // if (yelpStart[i] === yelpStart[x].ors){
         startPoints.push(yelpStart[i]);
     }
-    // }
+ 
     console.log("startPoints: ", startPoints);
-    
-    // }
+    yelpStartPoint.push(yelpStart[0]);
 }
 
-
-
 solveForStartPoints(geoObj, yelpObj);
+
+//finds which quadrant start point is in relative to end point
+function findQuadrant(endLat, endLong, yelpStartPoint) {
+    console.log("yelpstartpoint: ", yelpStartPoint);
+    
+    if (yelpStartPoint[0].coordinates.latitude < endLat && yelpStartPoint[0].coordinates.longitude > endLong) {
+        quadrant4(endLat, endLong, yelpStartPoint, yelpObj);
+    } else if (yelpStartPoint[0].coordinates.latitude < endLat && yelpStartPoint[0].coordinates.longitude < endLong) {
+        console.log("endLat: ", endLat, " endLong: ", endLong, " startLat: ", yelpStartPoint[0].coordinates.latitude, " startLong: ", yelpStartPoint[0].coordinates.longitude,)
+        quadrant3(endLat, endLong, yelpStartPoint, yelpObj);
+    } else if (yelpStartPoint[0].coordinates.latitude > endLat && yelpStartPoint[0].coordinates.longitude < endLong) {
+        quadrant2(endLat, endLong, yelpStartPoint, yelpObj);
+    } else {
+        quadrant1(endLat, endLong, yelpStartPoint, yelpObj);
+    }
+}
+
+findQuadrant(endLat, endLong, yelpStartPoint);
+
+function quadrant4(endLat, endLong, yelpStartPoint, yelpObj) {
+    for (i = 0; i < yelpObj[0].businesses.length; i++) {
+        if ((yelpObj[0].businesses[i].coordinates.latitude < endLat) && (yelpObj[0].businesses[i].coordinates.longitude > endLong)) {
+            candidates.push(yelpObj[0].businesses[i]);
+        }
+    }
+    console.log("candidates4: ", candidates);
+    optimizer(candidates, shortList, yelpStartPoint);
+}
+
+function quadrant3(endLat, endLong, yelpStartPoint, yelpObj) {
+    for (i = 0; i < yelpObj[0].businesses.length; i++) {
+        if ((yelpObj[0].businesses[i].coordinates.latitude < endLat) && (yelpObj[0].businesses[i].coordinates.longitude < endLong)) {
+            candidates.push(yelpObj[0].businesses[i]);
+        }
+    }
+    optimizer(candidates, shortList, yelpStartPoint);
+    console.log("candidates3: ", candidates);
+}
+
+function quadrant2(endLat, endLong, yelpStartPoint, yelpObj) {
+    for (i = 0; i < yelpObj[0].businesses.length; i++) {
+        if ((yelpObj[0].businesses[i].coordinates.latitude > endLat) && (yelpObj[0].businesses[i].coordinates.longitude < endLong)) {
+            candidates.push(yelpObj[0].businesses[i]);
+        }
+    }
+    console.log("candidates2: ", candidates);
+    optimizer(candidates, shortList, yelpStartPoint);
+}
+
+function quadrant1(endLat, endLong, yelpStartPoint, yelpObj) {
+    for (i = 0; i < yelpObj[0].businesses.length; i++) {
+        if ((yelpObj[0].businesses[i].coordinates.latitude > endLat) && (yelpObj[0].businesses[i].coordinates.longitude > endLong)) {
+            candidates.push(yelpObj[0].businesses[i]);
+        }
+    }
+    console.log("candidates1: ", candidates);
+    optimizer(candidates, shortList, yelpStartPoint);
+}
+
+//optimizes list to ensure the results match the desired number of bars
+function optimizer(candidates, shortList, startInput) {
+    console.log("startInput: ", startInput);
+    if (candidates.length < barNumber) {
+        console.log("bars <");
+        for (i = 0; i < (barNumber - candidates.length); i++) {
+            var shortListSelection = shortList[Math.floor(Math.random() * (shortList.length-1))];
+            if (startInput[0].id !== shortListSelection.id){
+                candidates.push(shortListSelection);
+            }
+        }
+        console.log("candidates <: ", candidates);
+    } else if (candidates.length > barNumber) {
+        console.log("bars >");
+        var candidatesTemp = [];
+        candidatesTemp.unshift(startInput[0]);
+        for (i = 0; i < barNumber-1; i++) {
+            console.log("candidatesTemp 1st");
+            
+            for (x = 0; x < candidatesTemp.length; x++) {
+                var shortListSelection = candidates[Math.floor(Math.random() * (candidates.length-1))];
+                console.log("candidatesTemp 2nd");
+                if ((startInput[0].id !== shortListSelection.id) && candidatesTemp[x].id !== shortListSelection.id) {
+                    candidatesTemp.push(shortListSelection);
+                }
+            }
+        }
+        candidates = candidatesTemp;
+        // candidates.unshift(startInput[0]);
+        console.log("candidates >: ", candidates);
+    }
+}
+
 
 //append results to DOM
 
 //take desired start point and iterate though decreasing distances between startpoint and endpoint to find other bars (distance//bars)
 
 //append route results to DOM
-// Persona'
-// 'The Lark Bar'
-// 'Bourbon & Branch'
+
+//event listeners
+
+//initialize page
